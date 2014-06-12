c4b80aede7d8325f9112b228b93373d64470d46e
Michael Hidalgo <michaelfallas@gmail.com>
2014-06-12 11:07:42 -0600

    Update config.inc.php

--- config/config.inc.php
+++ config/config.inc.php
@@ -14,8 +14,8 @@ $DBMS = 'MySQL';
 # Please use a database dedicated to DVWA.
 
 $_DVWA = array();
-$_DVWA[ 'db_server' ] = 'localhost';
-$_DVWA[ 'db_database' ] = 'MySQL';
+$_DVWA[ 'db_server' ] = 'us-cdbr-azure-east-a.cloudapp.net';
+$_DVWA[ 'db_database' ] = 'dvwa';
 $_DVWA[ 'db_user' ] = 'be1211b985946e';
 $_DVWA[ 'db_password' ] = '58dfb200';
 
